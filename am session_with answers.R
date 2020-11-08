b32ffaf773fb4ed83f907682d60dcd779fc79b6a
Iva Čukić <ivacukic@gmail.com>
2020-11-08 20:13:58 +0000

    Add files via upload

---
+++ am session_with answers.R
@@ -0,0 +1,149 @@
+### MORNING SESSION ###
+
+#Set working directory
+
+# your own path to folder
+setwd("~/Dropbox/R Course/") # Mac
+setwd("C:/Users/Iva Cukic/Dropbox/R Course/") # Windows
+setwd("/Users/1033123/Dropbox/R Course/") # Managed Windows
+
+
+# Read in data
+data <- read.csv("am_data.csv", header=TRUE) 
+
+#Inspect the data set
+head(data)
+names(data)
+View(data)
+
+# variable types
+is.factor(data$sex) # is it categorical?
+is.factor(data$age)
+is.numeric(data$mmse) # is it continuous?
+is.numeric(data$sex)
+data$sex <- as.factor(data$sex)
+
+# how many cases per category?
+table(data$sex) # 1 == Men 2 == Women
+table(data$disability) # 0 == absent 1 == present
+table(data$sex, data$disability)
+
+# how about numeric variables?
+summary(data$age)
+summary(data$mmse)
+summary(data$ces.d)
+
+### Basic manipulation ###
+
+# rename a variable
+names(data)
+names(data)[1] <- "id" #telling R to go to the first variable, and give it name "id"
+names(data)
+
+# rename all variables
+names(data) <- c("id","sex","age", "cognition", 
+                 "depression", "disability")
+names(data)
+
+# rename levels of a factor
+data$sex <- factor(data$sex, levels = c(1, 2), 
+                   labels = c("Male", "Female"))
+levels(data$sex) # 1 = Male, 2 = Female
+table(data$sex)
+
+# select only data for men
+men <- subset(data, data$sex=="Male")
+
+# DIY task:
+# select only data for women
+women <- subset(data, data$sex=="Female")
+ 
+# select only data for non-disabled men
+ndmen <-  subset(data, data$sex=="Male" & data$disability==1)
+
+# what is the age range?
+summary(data$age)
+
+#change the odd value into NA
+summary(data$age)
+is.na(data$age) = data$age == 219
+summary(data$age)
+
+# How many NAs in cognition 
+# and depression variables?
+summary(data$cognition)
+summary(data$depression)
+
+# What is the range for cognition and 
+# deperession scores? 
+summary(data$cognition)
+summary(data$depression)
+
+#check distributions for continuous variables
+hist() #variable you want to check in parentheses
+
+#split into categories according to clinical cut-offs
+
+#greater or equal 16 clinical depression
+data$dep_cat <- NA
+data$dep_cat[(data$depression>=16)] <- 1
+data$dep_cat[(data$depression<16)] <- 0
+table(data$dep_cat)
+
+#DIY:make a categorical variable for cognition according to
+# clinical cut off scores: 
+#24-30 no impairment; 18-23 mild; 0-17 severe cognitive impairment
+
+data$cog_cat <- NA
+data$cog_cat[(data$cognition <= 17)] <- 3
+data$cog_cat[(data$cognition > 17 & data$cognition <= 23  )] <- 2
+data$cog_cat[(data$cognition > 23  )] <- 1
+table(data$cog_cat)
+
+data$cog_cat <- factor(data$cog_cat, levels = c(1, 2, 3), 
+                   labels = c("No impairment", "Mild impairment", "Severe impairment"))
+table(data$cog_cat)
+
+#scatterplot cognition and depression
+plot(data$cognition, data$depression)
+?(plot) #see help for this function to see what else you can do
+# ?() asks for help for any function, write the function in ()
+#add labels to x and y axes
+plot(data$cognition, data$depression, xlab="Cognition", ylab="Depression")
+
+#save as image in your working directory
+png(filename="first-plot.png")
+plot(data$cognition, data$depression, xlab="Cognition", ylab="Depression")
+dev.off()
+
+#export an excel spreadsheet that contains new and changed variables
+write.csv(data, file = "new_data.csv")
+
+### correlation coefficient ###
+
+install.packages("Hmisc")
+library("Hmisc")
+cor.test(data$cognition, data$depression)
+
+
+### table of descriptives ###
+
+install.packages("tableone")
+library(tableone)
+
+#continuous vars
+tablevars <- c("age", "cognition", "depression")
+tableOne <- CreateTableOne(vars=tablevars, strata=c("sex"),data=data) #stratified by sex
+tableOne
+
+tableOne <- CreateTableOne(vars=tablevars,data=data) #total
+tableOne
+
+#categorical vars
+catVars <- c("disability")
+catTableOverall <- CreateCatTable(vars = catVars,strata=c("sex"), data = data) #stratified by sex
+catTableOverall
+
+catTableOverall <- CreateCatTable(vars = catVars, data = data) #total
+catTableOverall
+
---
+++ pm session.R
@@ -0,0 +1,146 @@
+### PM SESSION ###
+
+# read in new data
+newdata <- read.csv("pm_data.csv", header=TRUE)
+#have a look what's in this dataset, examine the variables
+
+# merge two data frames
+data <- read.csv("am_data.csv", header=TRUE) #read in morning dataset
+merged_data <- merge(data, newdata, by="id", all=TRUE)
+
+#oops, looks like an error message! can you tell what went wrong?
+
+names(data)
+names(newdata)
+#examine both id variables
+# there is one wrong id value, substitute with the correct one
+# data.frame[row_number, column_number] = new_value
+
+data
+data[20,1] = 20
+data
+
+#another way using variable (column) name
+data$id[20]=20 #row number in square brackets
+
+#we also need to rename one of the id variables to match the other
+names(newdata)[1] <- "id" #renaming the id variable in the second data frame to match the first
+
+# now we can finally merge
+merged_data <- merge(data, newdata, by="id", all=TRUE) # all.x T; all.y=T
+
+names(merged_data)
+head(merged_data)
+
+# create data frame with no missing values
+final_data <- na.omit(merged_data)
+
+# independent t-test
+# generic formula
+model <- t.test(outcome ~ predictor, data = mydata, paired= T/F)
+model
+
+# Do depression scores differ between men and women?
+m1 <- t.test(depression ~ sex, data = final_data)
+m1
+
+# Do cognition scores differ between those with and without disability?
+
+# calculate the effect size
+model <- t.test(depression ~ sex, data=final_data) #run the model
+t <- model$statistic[[1]] # extract the value of t
+df <- model$parameter[[1]] # extract degrees of freedom
+r <- sqrt(t^2/(t^2+df)) #calculate the effect size r based on t and df
+round(r, digits=2) #round to two decimal places
+
+# ANOVA
+model2 <- aov(depression ~ group, data=final_data)
+summary(model2)
+plot(model2)
+
+#effect size e.g. omega squared can be calculated based on the values provided in the summary()
+# posthoc tests
+pairwise.t.test(final_data$depression, final_data$group, paired=FALSE, p.adjust.method = "bonferroni")
+
+### Regression ###
+# continuous outcome
+# does age predicts cognition
+m1 <- lm(cognition ~ age, data=final_data)
+summary(m1)
+
+#if we want to standardise a variable for ease of interpretation
+m2 <- lm(scale(cognition) ~ age, data=final_data)
+summary(m2)
+
+# does age predict cognition, controlling for sex and disability
+m3 <- lm(scale(cognition) ~ age + as.factor(sex) + as.factor(disability), data=final_data)
+summary(m3)
+
+# does age predict depression
+m3 <- lm(scale(depression) ~ age, data=final_data)
+summary(m3)
+
+# does age predict depression, controlling for sex and disability
+m4 <- lm(scale(depression) ~ age + as.factor(sex) + as.factor(disability), data=final_data)
+summary(m4)
+
+
+# binary outcome: glm function
+# use the categorical depression variable we created earlier, dep_cat
+
+m5 <- glm(dep_cat ~ scale(age), data = final_data, family = "binomial") 
+summary(m5)
+
+# does cognition score predict depression?
+m6 <- glm(dep_cat ~ cognition, data = final_data, family = "binomial") 
+summary(m6)
+
+# remove everything from the environment
+rm(list=ls())
+
+### Exercise ###
+
+# read in first dataset week4dataa
+data2 <- read.csv("exercise_data_a.csv", header=TRUE) 
+
+# inspect variables (variable type, range/frequency, distribution/histograms, 
+#weird values, NAs)
+# clean up if needed
+
+names(data2) # see what variables are there
+View(data2) # show dataframe as spreadsheet
+summary(data2$id) #check if there are any weird values in first variable
+summary(data2$age) # 3 NAs
+table(data2$sex) 
+summary(data2$bmi) # weird values, need to inspect further
+hist(data2$bmi)
+is.na(data2$bmi) = data2$bmi == -999
+is.na(data2$bmi) = data2$bmi == 804
+summary(data2$bmi) # still one unlikely value
+is.na(data2$bmi) = data2$bmi == 7
+summary(data2$bmi) # looking OK now
+
+
+# assign labels to factor levels
+
+# read in second dataset, exercise_data_b, repeat like for dataset 1
+data3 <- read.csv("exercise_data_b.csv", header=TRUE)
+  
+# recode anxiety variable into a new one with three categories:
+# 0-3 no anxiety; 4-6 moderate; 7+ severe; assign labels
+
+# prepare both datasets for merging
+# merge data sets
+
+# main analyses:
+# produce table of descriptives for all variables in the study, stratified by dementia status and total
+# what is the correlation between bmi and iq. produce scatterplot. 
+# does bmi differ between those with and without dementia
+# do mean levels of anxiety differ between treatment groups (1 no treatment; 2 placebo; 3 intervention)
+# does iq predict dementia?
+# does iq predict dementia, controlling for age, sex and bmi?
+
+
+
+
+
